3d8b4ab50c37fac22bd34c186d0adb81514ffb54
EditedCocktail <72161012+EditedCocktail@users.noreply.github.com>
2022-07-08 16:14:36 +0500

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from mcstats import mcstats
 
 info = {
-	"ip":"51.161.84.142",
+	"ip":"editedworld.mcnetwork.me",
 	"port":41024,
 	"motd":"EditedWorld",
 	"version":"1.19.2"
